cd6c691f41b838fa63bdd35478817f349682c3ef
Tyler Johnson <tyler@livefront.com>
2019-04-11 12:07:10 -0500

    Accept user input for start and end times

--- BabysitterCalculator/Sources/BabysitterCalculator/main.swift
+++ BabysitterCalculator/Sources/BabysitterCalculator/main.swift
@@ -1,1 +1,35 @@
-print("Hello, world!")
+import Foundation
+
+func getEntry() -> String? {
+    let keyboard = FileHandle.standardInput
+    let inputData = keyboard.availableData
+    let string = String(data: inputData, encoding: String.Encoding.utf8)
+    return string?.trimmingCharacters(in: CharacterSet.newlines)
+}
+
+print("Enter start hour: ")
+guard let startingHourString = getEntry() else {
+    print("Oops. Invalid start hour")
+    exit(1)
+}
+
+print("Enter end hour: ")
+guard let endingHourString = getEntry() else {
+    print("Oops. Invalid end hour")
+    exit(1)
+}
+
+let calculator = PaymentCalculator(strategy: .a)
+
+do {
+    let shift = try Shift.makeShift(startTimeString: startingHourString, endTimeString: endingHourString)
+
+    switch calculator.calculatePayment(forShift: shift) {
+    case .success(let payment):
+        print("Payment: \(payment)")
+    case .failure(let error):
+        print("Error: \(error)")
+    }
+} catch ShiftValidationError.invalidEntry(message: let message) {
+    print(message)
+}
